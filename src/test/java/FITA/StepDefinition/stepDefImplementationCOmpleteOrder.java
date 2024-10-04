af3bc6f9b53b7826cba6dcc2c157284a6628e523
unknown <qaakramsheriff@gmail.com>
2024-10-04 08:45:33 +0530

    First Test Commit

--- src/test/java/FITA/StepDefinition/stepDefImplementationCOmpleteOrder.java
+++ src/test/java/FITA/StepDefinition/stepDefImplementationCOmpleteOrder.java
@@ -21,41 +21,41 @@ public class stepDefImplementationCOmpleteOrder extends BaseTest{
 	public CartPage cartPage;
 	public CheckoutPage checkoutPage;
 	
-	@Given("^Logged in with username(.+) and password (.+)")
-	public void Logged_in_with_username_and_password(String username,String password) throws IOException
-	{
-		landingpage=launchapplication();
-		landingpage.loginApplication(username, password);
-	}
-	
-	
-	@When("I add product to cart")
-	public void i_add_product_to_cart() {
-	    // Write code here that turns the phrase above into concrete actions		
-//		productspage.selectingProduct();
-        if (this.productspage != null) {
-            productspage.selectingProduct();
-        } else {
-            throw new RuntimeException("ProductsPage is not initialized");
-        }
-	    throw new io.cucumber.java.PendingException();
-	}
-	
-	
-	@When("checkout and subit the orders")
-	public void checkout_and_subit_the_order() throws InterruptedException {
-	    // Write code here that turns the phrase above into concrete actions
-		cartPage.Clickcartcheckout();
-	    throw new io.cucumber.java.PendingException();
-	}
-	
-	
-	@Then("Get the confirmation messages")
-	public void get_the_confirmation_message() {
-	    // Write code here that turns the phrase above into concrete actions
-		checkoutPage.checkout();
-	    throw new io.cucumber.java.PendingException();
-	}
+//	@Given("^Logged in with username(.+) and password (.+)")
+//	public void Logged_in_with_username_and_password(String username,String password) throws IOException
+//	{
+//		landingpage=launchapplication();
+//		landingpage.loginApplication(username, password);
+//	}
+//	
+//	
+//	@When("I add product to cart")
+//	public void i_add_product_to_cart() {
+//	    // Write code here that turns the phrase above into concrete actions		
+////		productspage.selectingProduct();
+//        if (this.productspage != null) {
+//            productspage.selectingProduct();
+//        } else {
+//            throw new RuntimeException("ProductsPage is not initialized");
+//        }
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//	
+//	
+//	@When("checkout and subit the orders")
+//	public void checkout_and_subit_the_order() throws InterruptedException {
+//	    // Write code here that turns the phrase above into concrete actions
+//		cartPage.Clickcartcheckout();
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//	
+//	
+//	@Then("Get the confirmation messages")
+//	public void get_the_confirmation_message() {
+//	    // Write code here that turns the phrase above into concrete actions
+//		checkoutPage.checkout();
+//	    throw new io.cucumber.java.PendingException();
+//	}
 	
 	
 	
